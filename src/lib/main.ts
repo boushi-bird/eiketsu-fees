ff615cadf1402d9243ef1e18fc8a7b32fd5b3b82
boushi-bird <boushi-bird@users.noreply.github.com>
2022-12-27 23:55:23 +0900

    文言と回数表示変更

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -8,10 +8,16 @@ function getData() {
   }
 }
 
+function numberFormat(value: number) {
+  return value.toLocaleString('ja-JP');
+}
+
 export function main() {
   const data = getData();
   if (!data) {
-    alert('英傑大戦.netのデータリストページではない。または読み込み中の可能性があります。');
+    alert(
+      '英傑大戦.netのデータリストページで実行してください。読み込み中であれば少し待ってから実行してください。'
+    );
     return;
   }
   const playerData = data.playerData?.[0];
@@ -24,8 +30,8 @@ export function main() {
     const totalPlayNum = playerData.total_play_num;
     const totalPayment = (totalPlayNum + totalGift) * ONE_CREDIT;
     const message = `英傑大戦の総プレイ料金は
-【 ${totalPayment.toLocaleString('ja-JP')} 円 】です。
-総プレイ回数: ${totalPlayNum} 回、総贈り物回数: ${totalGift} 回
+【 ${numberFormat(totalPayment)} 円 】です。
+総プレイ回数: ${numberFormat(totalPlayNum)} 回、総贈り物回数: ${numberFormat(totalGift)} 回
 
 ※総プレイ料金は総プレイ回数、総贈り物回数からの予想値です。1クレジット${ONE_CREDIT}円で計算しています。
 ※印刷料金は含んでいません。`;
