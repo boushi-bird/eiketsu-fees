3b33cb69633803e13c927cd25e2c3112d4a788d2
boushi-bird <boushi-bird@users.noreply.github.com>
2022-12-28 12:52:42 +0900

    非公式ツールであることを強調するように修正

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -12,17 +12,22 @@ function numberFormat(value: number) {
   return value.toLocaleString('ja-JP');
 }
 
+function show(message: string) {
+  alert(`[総プレイ料金ブックマークレット(非公式ツール)]
+${message}`);
+}
+
 export function main() {
   const data = getData();
   if (!data) {
-    alert(
+    show(
       '英傑大戦.netのデータリストページで実行してください。読み込み中であれば少し待ってから実行してください。'
     );
     return;
   }
   const playerData = data.playerData?.[0];
   if (!playerData) {
-    alert('英傑大戦.netにログインした状態で実行してください。');
+    show('英傑大戦.netにログインした状態で実行してください。');
     return;
   }
   try {
@@ -31,13 +36,14 @@ export function main() {
     const totalPayment = (totalPlayNum + totalGift) * ONE_CREDIT;
     const message = `英傑大戦の総プレイ料金は
 【 ${numberFormat(totalPayment)} 円 】です。
-総プレイ回数: ${numberFormat(totalPlayNum)} 回、総贈り物回数: ${numberFormat(totalGift)} 回
+総プレイ回数: ${numberFormat(totalPlayNum)} 回
+総贈り物回数: ${numberFormat(totalGift)} 回
 
 ※総プレイ料金は総プレイ回数、総贈り物回数からの予想値です。1クレジット${ONE_CREDIT}円で計算しています。
 ※印刷料金は含んでいません。`;
-    alert(message);
+    show(message);
   } catch (e) {
     console.error(e);
-    alert('エラーが発生しました。このブックマークレットは使えなくなった可能性があります。');
+    show('エラーが発生しました。このブックマークレットは使えなくなった可能性があります。');
   }
 }
